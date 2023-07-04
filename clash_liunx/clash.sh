ca8e9e0d71a29ca8f6a931817c4bc3e21a20c115
tyl2102003 <2114098434@qq.com>
2023-07-04 10:24:11 +0800

    new commit

--- clash_liunx/clash.sh
+++ clash_liunx/clash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 设置网路代理
+# 设置网路代理(确保网络代理是开启手动状态）
 export ALL_PROXY=socks://127.0.0.1:7891/
 export all_proxy=socks://127.0.0.1:7891/
 export http_proxy=http://127.0.0.1:7890/
@@ -8,7 +8,7 @@ export no_proxy=localhost,127.0.0.0/8,::1
 
 echo "proxy set success"
 # 启动clash
-cd ./clash/clash
+cd ./clash
 echo "starting...."
 ./clash -d .
 echo "启动成功"
